9120b90ad60524c59e84f520991b4f4464fb27bb
Collin <cdchamberlin.rva@gmail.com>
2023-10-20 20:40:25 -0400

    Initial set up

--- script.js
+++ script.js
@@ -67,3 +67,25 @@ const getGreeting2 = name => 'Hello ' + name + '!'
 //console.log(getGreeting("Collin"))
 // console.log(getGreeting1('Collin'))
 // console.log(getGreeting2(`Collin`))
+
+// Question 6 given code
+const westley = {
+    name: 'Westley',
+    numFingers: 5
+    }
+    const rugen = {
+    name: 'Count Rugen',
+    numFingers: 6
+    }
+    const inigo = {
+    firstName: 'Inigo',
+    greeting(person) {
+    let greeting = `Hello ${person.name}, my name is ${this.firstName}. `;
+    console.log(greeting + this.getCatchPhrase(person));
+    },
+    getCatchPhrase(person) {
+    return 'Nice to meet you.';
+    }
+    }
+    inigo.greeting(westley)
+    inigo.greeting(rugen)
